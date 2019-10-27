98423e51a4d86c610b186433a4e734f264aa0190
William Stevens <wstevens.hornet@gmail.com>
2019-10-27 06:59:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,11 @@ class Song
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1]
     artist = filename.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist 
+    n_song = self.new(song)
+    n_song.artist_name = artist 
+    n_song
   end
+  
   
   
   
